8ab8b5def2ebdc758126dc9a9a7c462ca6f20e8e
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-10-30 16:35:53 +0530

    done with todo list app

--- todo-list/src/components/TodoForm.jsx
+++ todo-list/src/components/TodoForm.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Usetodo } from "../context";
 
 const TodoForm = () => {
-  return (
-    <div>TodoForm</div>
-  )
-}
+  const [todos, setTodod] = useState([]);
+  const [addTodo] = Usetodo();
 
-export default TodoForm
+  const add = (e) => {
+    e.preventDefault();
+    if (!todos) return;
+    addTodo({ todos, completed: false });
+
+    setTodod("")
+  };
+  return (
+    <form onSubmit={add} className="flex">
+      <input
+        type="text"
+        
+        placeholder="Write Todo..."
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+      value={todos}
+      onChange={(e)=>setTodod(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+      >
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default TodoForm;
