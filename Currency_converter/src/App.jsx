10df4b9ec8c6a40906b2ff276e143e900e169575
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-07-31 18:48:40 +0530

    code fix

--- Currency_converter/src/App.jsx
+++ Currency_converter/src/App.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+const[amount , setAmount] = useState(0);
+const [from , setFrom] = useState("usd");
+const [to , setTo]= useState("inr");
+const[convertedAmount , setConvertedAmount]=useState(0)
+
+
   return (
     <div className=''>
       <h1 className='text-center bg-red-500'>Rishabh don't Give up</h1>
